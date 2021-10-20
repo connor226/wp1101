033b7ffe43c18e910ff8e3984856ab404c1b3140
connor226 <connor.taipei@gmail.com>
2021-10-20 10:05:02 +0800

    hw4 init

---
+++ hw4/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Counter from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
